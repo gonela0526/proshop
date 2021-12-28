c6f67c4c1bbd6610cc7c89b738a1bf1b9e736564
gonela0526 <gonela.sumanth@gmail.com>
2021-12-28 20:49:42 +0530

    header&footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     < >
-     <h1>Welcome to the proshop</h1>
+     <Header />
+    <main class="py-3">
+      <Container>
+      <h1>Welcome to the proshop.</h1>
+        </Container>
+    
+    </main>
+    <Footer />
+     
     </>
   );
 }
